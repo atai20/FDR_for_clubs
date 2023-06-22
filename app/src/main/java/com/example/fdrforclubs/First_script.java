d635e81953254af649c27d3a1cf94bca379619f0
atai20 <37374053+atai20@users.noreply.github.com>
2023-06-22 14:06:15 -0400

    Initial commit

---
+++ app/src/main/java/com/example/fdrforclubs/First_script.java
@@ -0,0 +1,31 @@
+package com.example.fdrforclubs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class First_script extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_page);
+        Button button = (Button) findViewById(R.id.button1);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent main_page = new Intent (First_script.this, MainActivity.class);
+                startActivity(main_page);
+            }
+        });
+    }
+}
