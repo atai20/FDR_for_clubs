b2fd6435d9a7c234dcbb15a74de2d56b38dd4278
atai20 <37374053+atai20@users.noreply.github.com>
2023-06-23 16:17:38 -0400

    data is can be seen now

--- app/src/main/java/com/example/fdrforclubs/MainActivity.java
+++ app/src/main/java/com/example/fdrforclubs/MainActivity.java
@@ -4,8 +4,13 @@ import static android.content.ContentValues.TAG;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -19,24 +24,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button1 = (Button)findViewById(R.id.send_button);
         // Write a message to the database
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("message");
+        EditText base_mess = (EditText) findViewById(R.id.editTextText);
 
-        myRef.setValue("Hello, World!");
+
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                TextView text1 = (TextView) findViewById(R.id.textView2);
+                myRef.setValue(text1.getText().toString());
+            }
+        });
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 // This method is called once with the initial value and again
                 // whenever data at this location is updated.
                 String value = dataSnapshot.getValue(String.class);
-                Log.d(TAG, "Value is: " + value);
+                base_mess.setText("Value is: " + value);
             }
 
             @Override
             public void onCancelled(DatabaseError error) {
                 // Failed to read value
-                Log.w(TAG, "Failed to read value.", error.toException());
+                base_mess.setText("Failed to read value.");
             }
         });
     }
